0309a077c7db5af41735b417a38924ae32f9eac5
Jesse Brink <jbrink0925@gmail.com>
2024-11-01 09:56:00 -0400

    Add Github Actions script to project.

---
+++ setup-test-db.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Database connection details
+DB_NAME="postgres"
+DB_USER="postgres"
+DB_HOST="localhost"
+DB_PORT="5432"
+
+# SQL commands to create tables
+SQL_COMMANDS="
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(20) NOT NULL,
+    password VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS projects (
+    id            SERIAL PRIMARY KEY,
+    directories   JSONB,
+    project_name  VARCHAR(100) NOT NULL,
+    user_id       INTEGER NOT NULL,
+    created_at    BIGINT NOT NULL,
+    mtime         BIGINT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS files (
+    id            SERIAL PRIMARY KEY,
+    name          VARCHAR(20),
+    path          VARCHAR(150),
+    project_name  VARCHAR(100),
+    data          BYTEA NOT NULL,
+    user_id       INTEGER NOT NULL,
+    project_id    INTEGER NOT NULL,
+    mtime         BIGINT NOT NULL,
+    created_at    BIGINT NOT NULL,
+    FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS shared (
+    id            SERIAL PRIMARY KEY,
+    hash          VARCHAR(100) NOT NULL,
+    user_id       INTEGER NOT NULL,
+    project_id    INTEGER NOT NULL,
+    file_name     VARCHAR(20) NOT NULL,
+    project_name  VARCHAR(100) NOT NULL,
+    FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+"
+
+# Execute SQL commands using psql
+psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -c "$SQL_COMMANDS"
